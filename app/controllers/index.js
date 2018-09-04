f6dc9a880fe8ee23be0f7fbe3d64068fe72147f4
juliokhichfy <juliocesark@gmail.com>
2018-09-04 01:42:52 -0300

    JSON PARSER: ok

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,30 +1,10 @@
-module.exports.home = function(app, req, res){
+module.exports.home = function(app, req, res) {
 	console.log("passei no controler home");
 	res.render('index.ejs',{name_autor : 'Julio Cesar Khichfy' , eventsJson:''} );
 }
 
-module.exports.getEvents = function(app, req, res){
-	
+module.exports.getEvents = function(app, req, res) {
 	var events = req.body.eventsJson;
-	console.log(">>>>> events: " + events);
-	
-	json = JSON.stringify(events);
-	console.log("json typeof: " + typeof json);
-	console.log(">>>>> json: " + json);
-	console.log("json name: " + json.name);
-	
-	data = JSON.parse(json);
-	console.log("data typeof: " + typeof data);
-	console.log("data: " + data);
-	console.log("data typeof: " + data.name);
-
-	/*teste hard c*/
-	eventStr = JSON.stringify({name:"julio"});
-	datateste = JSON.parse(eventStr);
-	console.log(">>>>> stringify eventStr typeof : " +typeof datateste);
-	console.log(">>>>> stringify eventStr : " + datateste);
-	console.log(">>>>> stringify eventStr name : " + datateste.name);
-	
-	
-	res.render('index.ejs', {name_autor : 'Julio Cesar Khichfy', eventsJson : eventStr});
+	var obj = eval("(" + events + ')');
+	res.render('index.ejs', {name_autor : 'Julio Cesar Khichfy', eventsJson : JSON.stringify(obj) });
 }
